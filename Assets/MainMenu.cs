e8b52a7c62a10b8ee909c2e3b2810127a18f5560
Fvbvke <novoselcevila12@gmail.com>
2023-02-24 11:56:22 +0300

    Добавил все основные файлы проекта Unity.
    Добавил все основные файлы проекта Unity.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Update() {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void PlayGame() {
+        SceneManager.LoadScene("game", LoadSceneMode.Single);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class Agent : MonoBehaviour
+{
+    public GameObject player;
+    private NavMeshAgent agent;
+
+    private void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        agent.destination = player.transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        SceneManager.LoadScene(0);
+    }
+}
